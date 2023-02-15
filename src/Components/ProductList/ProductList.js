557cb7799d20813b2f6207f5a72ab7a8097690e4
Katarn <katarn@katarn.es>
2023-02-15 19:38:46 +0100

    ProductList styles

--- src/Components/ProductList/ProductList.js
+++ src/Components/ProductList/ProductList.js
@@ -1,17 +1,19 @@
 import {ProductListItem} from "./ProductListItem";
-
+import styles from "./ProductList.module.scss"
 /**
  * @param products {ProductSummary[]}
  */
 export function ProductList({ products }) {
     return (
-        <>{ products.map(product => {
-            return (
-                <ProductListItem
-                    key={product.id}
-                    product={product}
-                />
-            )
-        }) }</>
+        <div className={styles['product-list__container']}>
+            { products.map(product => {
+                return (
+                    <ProductListItem
+                        key={product.id}
+                        product={product}
+                    />
+                )
+            }) }
+        </div>
     )
 }
