c890790d2eae9bac86a9df22210d07ba96a62ea7
Katarn <katarn@katarn.es>
2023-02-18 19:37:57 +0100

    ProductListItem tests

--- src/Components/ProductList/ProductListItem.js
+++ src/Components/ProductList/ProductListItem.js
@@ -19,12 +19,23 @@ export function ProductListItem({ product }) {
                 {product.brand}
             </span>
             { !product.soldOut
-                ? <span className={styles['product-list-item__price']}>{product.humanPrice}</span>
-                : <span className={styles['product-list-item__sold-out']}>Agotado!</span>
+                ? <span
+                    className={styles['product-list-item__price']}
+                    data-testid="product-price"
+                >
+                    {product.humanPrice}
+                </span>
+                : <span
+                    className={styles['product-list-item__sold-out']}
+                    data-testid="product-sold-out"
+                >
+                    Agotado!
+                </span>
             }
             <Link
                 className={styles['product-list-item__link']}
                 to={`/products/${product.id}`}
+                data-testid="product-link"
             >
                 <span className="sr-only">
                     Ver producto
---
+++ src/Components/ProductList/ProductListItem.test.js
@@ -0,0 +1,68 @@
+import {ProductSummary} from "../../Entities/ProductSummary";
+import {ProductListItem} from "./ProductListItem";
+import {render, screen} from "@testing-library/react";
+import {BrowserRouter} from "react-router-dom";
+
+describe('src/Components/ProductList/ProductListItem.js', () => {
+    it('should have a link to the product', () => {
+        const productFixture = new ProductSummary(
+            'some-id',
+            'Some Brand',
+            'Some Model',
+            100,
+            'http://some.image',
+        )
+
+        render(
+            <BrowserRouter>
+                <ProductListItem
+                    product={productFixture}
+                />
+            </BrowserRouter>
+        )
+
+        expect(screen.getByTestId('product-link')).toHaveAttribute('href', '/products/some-id')
+    })
+
+    it('should have a price label when is not sold out', () => {
+        const productFixture = new ProductSummary(
+            'some-id',
+            'Some Brand',
+            'Some Model',
+            100,
+            'http://some.image',
+        )
+
+        render(
+            <BrowserRouter>
+                <ProductListItem
+                    product={productFixture}
+                />
+            </BrowserRouter>
+        )
+
+        expect(screen.getByTestId('product-price')).toHaveTextContent('100.00 €')
+        expect(screen.queryByTestId('product-sold-out')).toBeNull()
+    })
+
+    it('should have a sold out label when is sold out', () => {
+        const productFixture = new ProductSummary(
+            'some-id',
+            'Some Brand',
+            'Some Model',
+            null,
+            'http://some.image',
+        )
+
+        render(
+            <BrowserRouter>
+                <ProductListItem
+                    product={productFixture}
+                />
+            </BrowserRouter>
+        )
+
+        expect(screen.getByTestId('product-sold-out')).toHaveTextContent('Agotado!')
+        expect(screen.queryByTestId('product-price')).toBeNull()
+    })
+})
