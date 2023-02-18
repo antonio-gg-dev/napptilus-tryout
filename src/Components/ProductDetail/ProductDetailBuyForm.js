e99a4a5282fbe46df773fd4ce0112f5b0e49d98f
Katarn <katarn@katarn.es>
2023-02-18 11:03:30 +0100

    Finish buy form styles

--- src/Components/ProductDetail/ProductDetailBuyForm.js
+++ src/Components/ProductDetail/ProductDetailBuyForm.js
@@ -5,14 +5,16 @@ import styles from "./ProductDetailBuyForm.module.scss"
  */
 export function ProductDetailBuyForm ({ product }) {
     const [activeColorCode, setActiveColorCode] = useState(product.colors[0].code)
-    // const [activeStorageCode, setActiveStorageCode] = useState(product.storages[0].code)
+    const [activeStorageCode, setActiveStorageCode] = useState(product.storages[0].code)
 
     return (
-        <>
+        <div className={styles['product-detail-buy-form__form']}>
+            <h2 className={styles['product-detail-buy-form__color-title']}>
+                Selecciona tu color:
+            </h2>
             <div className={styles['product-detail-buy-form__color-picker']}>
                 { product.colors.map((color) => (
                     <label
-                        key={color.code}
                         className={`
                             ${styles['product-detail-buy-form__color-option']}
                             ${activeColorCode === color.code ? styles['product-detail-buy-form__color-option--active'] : ''}
@@ -20,6 +22,8 @@ export function ProductDetailBuyForm ({ product }) {
                         style={{
                             '--color': color.cssColor
                         }}
+                        key={color.code}
+                        title={color.name}
                     >
                         {color.name}
 
@@ -29,16 +33,39 @@ export function ProductDetailBuyForm ({ product }) {
                             name="color"
                             onChange={() => setActiveColorCode(color.code)}
                             checked={activeColorCode === color.code}
-                            title={color.name}
                         />
                     </label>
                 )) }
             </div>
-            { product.storages.map((storage) => (
-                <div key={storage.code}>
-                    { storage.name }
-                </div>
-            )) }
-        </>
+
+            <h2 className={styles['product-detail-buy-form__storage-title']}>
+                Selecciona tu almacenamiento:
+            </h2>
+            <div className={styles['product-detail-buy-form__storage-picker']}>
+                { product.storages.map((storage) => (
+                    <label
+                        key={storage.code}
+                        className={`
+                            ${styles['product-detail-buy-form__storage-option']}
+                            ${activeStorageCode === storage.code ? styles['product-detail-buy-form__storage-option--active'] : ''}
+                        `}
+                    >
+                        {storage.name}
+
+                        <input
+                            type="radio"
+                            value={storage.code}
+                            name="storage"
+                            onChange={() => setActiveStorageCode(storage.code)}
+                            checked={activeStorageCode === storage.code}
+                        />
+                    </label>
+                )) }
+            </div>
+
+            <button className={styles['product-detail-buy-form__buy-button']}>
+                Comprar!
+            </button>
+        </div>
     )
 }
